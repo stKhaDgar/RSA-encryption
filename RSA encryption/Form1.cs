2354abe30545da8d3cd0fd5d82b56d42074bf816
stKhaDgar <stmaktavish@gmail.com>
2018-04-06 17:15:58 +0300

    Added alphabet + some elements on form

--- RSA encryption/Form1.cs
+++ RSA encryption/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace RSA_encryption
 {
+
     public partial class Form1 : Form
     {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Алфавит, используемый в программе
+        char[] characters = new char[] 
+        { '$', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+          'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+          'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+          'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+          '8', '9', '0' };
+
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Кнопка "Зашифровать"
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
