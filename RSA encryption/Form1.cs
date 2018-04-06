ef74f448559837118d6f77c39d86993b641c51ab
stKhaDgar <stmaktavish@gmail.com>
2018-04-06 17:41:19 +0300

    Added button for Decrypt

--- RSA encryption/Form1.cs
+++ RSA encryption/Form1.cs
@@ -177,5 +177,29 @@ namespace RSA_encryption
 
             return result;
         }
+
+        // Функция для расшифрования строки при помощи алгоритма RSA
+        private string RSA_Dedoce(List<string> input, long d, long n)
+        {
+            string result = "";
+
+            BigInteger bi;
+
+            foreach (string item in input)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                int index = Convert.ToInt32(bi.ToString());
+
+                result += characters[index].ToString();
+            }
+
+            return result;
+        }
     }
 }
